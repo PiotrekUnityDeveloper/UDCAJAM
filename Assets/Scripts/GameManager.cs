38c87e82d5689e2c88760b82d763589d20f4116a
PiotrekUnityDeveloper <85606121+PiotrekUnityDeveloper@users.noreply.github.com>
2022-03-09 09:20:23 +0100

    Pause Menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.Rendering.Universal;
+using System.Collections;
 
 [ExecuteAlways]
 public class GameManager : MonoBehaviour
@@ -9,10 +10,27 @@ public class GameManager : MonoBehaviour
     public GameObject postfxObj;
     public bool PostfxOn;
     ScriptableRendererFeature AmbientOclussion;
+
+    //pause stuff
+    public bool canPause;
+
+    public bool isPaused;
+    public GameObject PauseMenu;
+
+    public PlayerMovement playermove;
+    public PlayerLook playerlook;
+    public Pickup pickup;
     // Start is called before the first frame update
     void Start()
     {
-        
+        canPause = false;
+        StartCoroutine(DelayPauseMenu());
+    }
+
+    public IEnumerator DelayPauseMenu()
+    {
+        yield return new WaitForSecondsRealtime(23);
+        canPause = true;
     }
 
     // Update is called once per frame
@@ -21,5 +39,48 @@ public class GameManager : MonoBehaviour
         AmbientOclussion = frwrdRenderer.rendererFeatures.Find(x => x.name == "AO");
         AmbientOclussion.SetActive(PostfxOn);
         postfxObj.SetActive(PostfxOn);
+
+        if(Input.GetKey(KeyCode.Backspace))
+        {
+            canPause = true;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused == false && canPause == true)
+            {
+                
+                playermove.canmove = false;
+                playerlook.canlook = false;
+                pickup.candrag = false;
+                PauseMenu.SetActive(true);
+                Time.timeScale = 0;
+                isPaused = true;
+            }
+        }
+
+        if(isPaused == true && Cursor.lockState != CursorLockMode.None)
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        if(isPaused == false && Cursor.lockState != CursorLockMode.Locked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    public void UnPauseGame()
+    {
+        if (isPaused == true)
+        {
+            playermove.canmove = true;
+            playerlook.canlook = true;
+            pickup.candrag = true;
+            PauseMenu.SetActive(false);
+            Time.timeScale = 1;
+            Cursor.lockState = CursorLockMode.Locked;
+            isPaused = false;
+        }
     }
 }
