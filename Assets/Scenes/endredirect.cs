7dcb95375e045369b5168b8ba0ab69208280eadb
PiotrekUnityDeveloper <85606121+PiotrekUnityDeveloper@users.noreply.github.com>
2022-03-12 20:01:24 +0100

    fixed some stuff

--- Assets/Scenes/endredirect.cs
+++ Assets/Scenes/endredirect.cs
@@ -9,7 +9,7 @@ public class endredirect : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("OpenSnakeInfo", 18);
+        Invoke("OpenSnakeInfo", 17f);
     }
 
     // Update is called once per frame
